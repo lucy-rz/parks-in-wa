73754ba231f8aed4b8c9ceb18fec15dd94185ca0
Lucy Ruiz <lucyruizwa@gmail.com>
2024-04-07 17:21:04 -0700

    implement oauth

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/auth/google', passport.authenticate(
+    'google',
+    {
+        scope: ['profile', 'email'],
+        //optional
+        prompt: 'select_account',
+    }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+    'google',
+    {
+        successRedirect: '/',
+        failureRedirect: '/',
+    }
+));
+
+router.get('/logout', function(req, res){
+    req.logout(function() {
+        res.redirect('/');
+    });
 });
 
 module.exports = router;
